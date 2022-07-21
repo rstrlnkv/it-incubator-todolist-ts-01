43db15ca280c3a8155fbf7916e71f6f9d7e004f9
r_strlnkv <r@strlnkv.com>
2022-07-21 22:28:40 +0300

    Final

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,23 @@ import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: "Hello World", isDone: true},
+        {id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false}
+    ]
+
     return (
-        <div className="App">
-        <Todolist/>
-        <Todolist/>
-        <Todolist/>
-        </div>
-    );
+    <div className="App">
+        <Todolist title="Whats to Learn" tasks={tasks1}/>,
+        <Todolist title="Songs" tasks={tasks2}/>,
+    </div>
+);
 }
 
 export default App;
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 
-export function Todolist() {
+type PropsType = {
+    title:string
+    tasks: Array<TaskType>
+}
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export function Todolist(props:PropsType) {
     return <div>
-        <h3>What to learn</h3>
+        <h3>{props.title}</h3>
         <div>
             <input/>
             <button>+</button>
         </div>
         <ul>
-            <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-            <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-            <li><input type="checkbox" checked={false}/> <span>React</span></li>
+            <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
         </ul>
         <div>
             <button>All</button>
